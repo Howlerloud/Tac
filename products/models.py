020b8c07dcacf7e473f167bbeaac0a3a780e8602
Howlerloud <howlerjr@hotmail.co.uk>
2025-11-17 15:04:20 +0000

    Added sizes to products Updated bag list to show sizes

--- products/models.py
+++ products/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
-# Create your models here.
 
-
-class Category (models.Model):
+class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -13,7 +11,7 @@ class Category (models.Model):
 
     def __str__(self):
         return self.name
-    
+
     def get_friendly_name(self):
         return self.friendly_name
 
@@ -23,6 +21,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
