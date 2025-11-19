6298b9bf3e57d4d41e16351b804d25c2b0e8ffc6
Howlerloud <howlerjr@hotmail.co.uk>
2025-11-19 23:04:07 +0000

    Added checkout views and templates Fixed settings having a spelling mistake

--- tacweb/settings.py
+++ tacweb/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'products',
     'shopbag',
     'checkout',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -60,21 +62,28 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tacweb.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'templates', 'allauth')
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.request', # required by allauth
+                'django.contrib.auth.context_processors.auth', 
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'shopbag.contexts.bag_contents',
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ],
         },
     },
