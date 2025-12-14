dccbadce3cde61e801e26870661727acfdccd56c
Howlerloud <howlerjr@hotmail.co.uk>
2025-12-14 12:49:05 +0000

    Added image alts that the admin can customize. Added new model "image_alt" Updated css to fix background clipping

--- checkout/models.py
+++ checkout/models.py
@@ -70,7 +70,7 @@ class OrderLineItem(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the lineitem total
+        Override the original save method to set the line item total
         and update the order total.
         """
         self.lineitem_total = self.product.price * self.quantity
--- products/models.py
+++ products/models.py
@@ -26,6 +26,7 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    image_alt = models.CharField(max_length=50, blank=True, help_text="Detailed description for users that struggle to see the image, please describe colour, style, target audience", default="Undefined")
 
     def __str__(self):
         return self.name
